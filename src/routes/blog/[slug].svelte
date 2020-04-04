d6f2d713f5730bb606117984fdf77ec0a96c8fae
spitchell <mr.spitchell@gmail.com>
2020-04-04 13:55:20 +0100

    tags

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -72,3 +72,16 @@
 <div class='content'>
 	{@html post.html}
 </div>
+
+{#if post.tags}
+	<ul>
+	{#each post.tags as tag}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a rel='prefetch' href='#'>{tag}</a></li>
+		<!-- needs to point at page of tags... or does it? -->
+	{/each}
+</ul>
+{/if}
--- static/_posts/testing-sapper-cms-1.md
+++ static/_posts/testing-sapper-cms-1.md
@@ -1,6 +1,8 @@
 ---
 layout: blog
-title: 'Testing sapper cms '
+title: "Testing sapper cms "
 date: 2020-04-04T09:55:22.998Z
+tags: ["1", "2", "3"]
 ---
+
 Here is some text!
