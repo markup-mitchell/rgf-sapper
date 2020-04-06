be533371103e87688d805dccf4b6c48bea8d2768
spitchell <mr.spitchell@gmail.com>
2020-04-06 17:41:45 +0100

    cloudinary worked striaght off the bat

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -24,9 +24,9 @@
       flex-basis: 300px;
       flex-grow: 1; 
       min-width: 300px;
-
+      overflow: hidden;
       border: thin solid #eee;
-      border-radius: 4px;
+      border-radius: 0.25rem;
       box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
       transition: all 0.2s ease-in-out;
    }
