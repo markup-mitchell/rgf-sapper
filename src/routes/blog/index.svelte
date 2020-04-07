b4d371a64a4311f8d2e1a2af3a2408d1508dc8ce
spitchell <mr.spitchell@gmail.com>
2020-04-08 00:16:30 +0100

    reduce grid gap for browser parity

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -35,7 +35,7 @@
    .grid {
          display: grid;
          grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-         grid-gap: 2rem;
+         grid-gap: 1.5rem;
       }
    
 	ul {
