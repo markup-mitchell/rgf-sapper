00c4ac0ddc871d698f10bf071b43b713a71ae334
spitchell <mr.spitchell@gmail.com>
2020-04-14 08:56:35 +0100

    first stab

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,10 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
 <script>
   import { onMount } from 'svelte';
   import TransitionWrapper from '../components/TransitionWrapper.svelte';
@@ -12,6 +19,41 @@
       });
     }
   });
+  import BlogCard from "../components/BlogCard.svelte";
+  import moment from "moment";
+
+   import { fade } from "svelte/transition";
+   
+   export let posts;
+   let tagPosts = posts;
+   let tagFilter = null;
+   let showTags = true;
+
+   // tags from each post get put in an array which is flattened with concat and duplicates eliminated by set
+   let allTagsArray = [].concat.apply([], posts.map(post => {
+      return post.tags;
+   })).sort();
+
+   let allTagsAlphabetised = new Set(allTagsArray);
+
+   function filterPosts(tag) {
+      tagPosts = posts.filter(post => post.tags.includes(tag));
+      setTagFilter(tag);
+   }
+
+   function setTagFilter(tag) {
+      tagFilter = tag;
+   }
+
+   function clearTagFilter() {
+      tagFilter = null;
+      tagPosts = posts;
+   }
+
+   function toggleTags() { 
+      showTags = !showTags;
+      console.log(showTags);
+   }
 </script>
 
 <style>
@@ -51,6 +93,127 @@
 			grid-template-columns: 1fr 1fr 1fr;
 		}
 	}
+      .grid {
+       
+         display: grid;
+         grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+         grid-gap: 2rem;
+      }
+   
+   h2 {
+      text-transform: capitalize;
+      font-weight: bold;
+      margin-bottom: 1.125rem;
+   }
+
+	ul {
+      margin: 0;
+      padding: 0;
+		line-height: 1.5;
+   }
+
+   li {
+      list-style-type: none;
+      display: inline-flex;
+   }
+   
+   a {
+      display: inline-flex;
+      text-decoration-line: none;
+   }
+
+   .page-header {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding-top: 2rem;
+      grid-column: 1/-1;
+   }
+
+   .tag-btn {
+      border: none;
+      font-size: var(--text-sm);
+      letter-spacing: 1px;
+      background: transparent;
+      border: solid thin var(--black);
+      border-radius: 2px;
+      color: var(--black);
+      padding: 0.5rem;
+      margin: 0.1rem;
+      transition: all 0.2s ease;
+      white-space: nowrap;
+   }
+
+   .tag-btn:hover {
+      background-color: #555;
+      color: white;
+      transition: all 0.2s ease;
+   }
+ 
+   /* .tags {
+      position: fixed;
+      top: 3rem;
+      right: 0.5rem;
+      max-width: 30%;
+      height: 100%;
+      text-align: right;
+   } */
+
+   .flex {
+      display: flex;
+      align-items: baseline;
+   }
+
+   svg {
+      transition: all 0.2s ease;
+   }
+
+   svg:hover {
+      fill: red;
+      transition: all 0.2s ease;
+   }
+
+   button {
+      border:  none;
+      background-color: transparent;
+   }
+
+   .clear-tag {
+      margin: 6px 0 0 6px;
+      padding: 0;
+   }
+
+   .clear-tag__icon {
+      height: var(--text-base);
+      opacity: 0.3;
+   }
+
+   .clear-tag__icon:hover {
+      fill: red;
+   }
+
+   button:focus {
+      outline: none;
+      /* this is bad. how to do focus? invert? v.solid line? */
+   }
+/* 
+   .shavings {
+      position: absolute;
+      top: 7rem;
+      left: -10rem;
+      bottom: 0;
+      opacity: 0.5;
+      z-index: -1;
+      transform: rotate(-20deg);
+	} */
+
+   @media (min-width: 945px) {
+      .page-header {
+         align-items: center;
+         margin-bottom: 3rem;
+      }
+   }
 </style>
 
 <svelte:head>
@@ -60,15 +223,63 @@
 
 <TransitionWrapper>
 <div class="grid">
-<h1>Rather Good Fatherhood</h1>
-<div class="text">
-<p>
-	Scientists are not sure yet exactly what the term "father" actually means - and if scientists don't know something, nobody does.
-</p>
-<p>
-	So it's up for grabs, basically, and this is me staking my claim. <strong>I will tell you</strong> what it means to <strong>me<strong> over the course of as many blog posts as I can be bothered to write.
-</p>
-<p><strong>You</strong> will digest these words of wisdom and internalise them, papering over whatever internal inconsistencies you discover using your own initiative and experience with self-deceit.</p>
-<p><strong>THAT'S THE DEAL, OK?</strong></p>
+
+<div class="blog-grid">
+<div class="page-header">
+   <h1>Blog</h1>
+      <div class="flex">
+         {#if tagFilter}
+            <h2>
+               {tagFilter}
+            </h2>
+           <button class="clear-tag" on:click={clearTagFilter}>
+               <svg class="clear-tag__icon" viewbox="0 0 20 20">
+                  <polygon points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
+               </svg>
+            </button>
+         {:else}
+            <h2>all posts</h2>
+         {/if}
+         <!-- <button class="tag-toggle" on:click={toggleTags}>
+            <svg width="30" height="30" viewbox="0 0 20 20">
+               {#if showTags}
+                  <polygon in:fade points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
+               {:else}
+                  <path in:fade d="M0,10 L0,2 L2,0 L10,0 L20,10 L10,20 L0,10 Z M4.5,6 C5.32842712,6 6,5.32842712 6,4.5 C6,3.67157288 5.32842712,3 4.5,3 C3.67157288,3 3,3.67157288 3,4.5 C3,5.32842712 3.67157288,6 4.5,6 Z"></path>
+               {/if}
+            </svg>
+         </button>  -->
+      </div>
+      <!-- <img class="shavings" src="https://res.cloudinary.com/dthkwbvgt/image/upload/v1586378624/shavings_ogvg1s.png" alt="pencil shavings"> -->
+   </div>
+ 
+{#if showTags}
+<div class="tags">
+   <h2>Tags</h2>
+   <ul>
+      {#each [...allTagsAlphabetised] as tag}
+         <li>
+            <button class="tag-btn" on:click={() => filterPosts(tag)}>{tag}</button>
+         </li>
+      {/each}
+   </ul>
+</div>
+{/if}
+	{#each tagPosts as {title,lede,date,featured_image,tags, slug}}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<!-- <li></li> -->
+      <a rel='prefetch' href='blog/{slug}'>
+		<BlogCard
+         {title}
+         {lede}
+         {date}
+         {featured_image}
+      />
+   </a>
+	{/each}
+
 </div>
 </TransitionWrapper>
